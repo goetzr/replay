d9b49c2c9f914a704818521ddab276c2b9edf86b
Russ Goetz <russgoetz@gmail.com>
2024-07-29 19:51:09 -0400

    Ditched the parse float macro I started last time. Instead add a parse_float function that should work. Next step is to use the parse_float function in the argument parsing function.

--- generate/src/main.rs
+++ generate/src/main.rs
@@ -1,5 +1,6 @@
 use clap::{Command, Arg, value_parser};
 use anyhow::bail;
+use std::fmt;
 
 fn main() {
     let duration = parse_args();
@@ -62,12 +63,7 @@ fn parse_args() -> anyhow::Result<CliArgs> {
     let starting_range = *matches.get_one::<u32>("starting_range").unwrap();
     let starting_azimuth = matches.get_one::<&str>("starting_azimuth").unwrap();
 
-    macro_rules! parse_f64 {
-        ($arg_name:ident) => {
-            let arg_var = concat_idents
-        };
-    }
-
+    // TODO: Pick up here. Use new parse_float method.
     let starting_azimuth_id = format!("'--{} <{}>'",
             starting_azimuth_arg.get_long().unwrap(),
             starting_azimuth_arg.get_value_names().unwrap()[0],
@@ -88,4 +84,78 @@ fn parse_args() -> anyhow::Result<CliArgs> {
             max_starting_azimuth,
         );
     };
+}
+
+struct F64Bound {
+    value: f64,
+    inclusive: bool,
+}
+
+impl F64Bound {
+    fn new(value: f64, inclusive: bool) -> Self {
+        Self { value, inclusive }
+    }
+}
+
+struct F64Range {
+    lbound: F64Bound,
+    ubound: F64Bound,
+}
+
+impl F64Range {
+    fn new(lbound: F64Bound, ubound: F64Bound) -> Self {
+        Self { lbound, ubound }
+    }
+
+    fn contains(&self, value: f64) -> bool {
+        let ok1 = match self.lbound {
+            F64Bound { value: lbound, inclusive: true } => value >= lbound,
+            F64Bound { value: lbound, inclusive: false } => value > lbound,
+        };
+
+        let ok2 = match self.ubound {
+            F64Bound { value: ubound, inclusive: true } => value <= ubound,
+            F64Bound { value: ubound, inclusive: false } => value < ubound,
+        };
+
+        ok1 && ok2
+    }
+}
+
+impl fmt::Display for F64Range {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self.lbound {
+            F64Bound { value: lbound, inclusive: true } => write!(f, "[{lbound}, ")?,
+            F64Bound { value: lbound, inclusive: false } => write!(f, "({lbound}, ")?,
+        };
+        match self.ubound {
+            F64Bound { value: ubound, inclusive: true } => write!(f, "{ubound}], ")?,
+            F64Bound { value: ubound, inclusive: false } => write!(f, "{ubound}), ")?,
+        };
+        Ok::<(), fmt::Error>(())
+    }
+}
+
+fn parse_float(arg: &Arg, arg_value: &str, range: &F64Range) -> anyhow::Result<f64> {
+    let arg_id = format!("'--{} <{}>'",
+            arg.get_long().expect("argument must have a long option set"),
+            arg.get_value_names().expect("argument must have a value name set")[0],
+    );
+    let Ok(arg_value) = arg_value.parse::<f64>() else {
+        bail!("invalid value '{}' for {}: {} is not a valid floating point double",
+            arg_value,
+            arg_id,
+            arg_value,
+        );
+    };
+    if !range.contains(arg_value) {
+        bail!("invalid value '{}' for {}: {} is not in the range {}",
+            arg_value,
+            arg_id,
+            arg_value,
+            range,
+        );
+    };
+
+    Ok(arg_value)
 }
